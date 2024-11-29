5bb9cf5799b21f4eaf8abfa98d97ea07cee50e2c
Lucas  Sköld <lucasskold49@gmail.com>
2024-11-29 20:12:31 +0100

    Add docs

--- Utilities/StatHelper.cs
+++ Utilities/StatHelper.cs
@@ -4,6 +4,11 @@ namespace Arathia.Utilities
 {
     public static class StatHelper
     {
+        /// <summary>
+        /// Converts a given stat value to a format of 1.x, where x is derived from the stat value.
+        /// </summary>
+        /// <param name="statValue">The stat value to be converted.</param>
+        /// <returns>A float in the format of 1.x, where x is derived from the stat value.</returns>
         public static float ConvertToOneXFormat(float statValue)
         {
             int numberOfDigits = statValue.ToString().Length;
